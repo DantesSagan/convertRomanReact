647ced33674cc95f2c4f57f781140dfec80d17f4
DantesSagan <mihaleksval@gmail.com>
2022-03-13 02:43:58 +0500

    checking

--- src/App.js
+++ src/App.js
@@ -54,18 +54,6 @@ export default function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
         <div style={{ display: 'grid', padding: '5px' }}>
           <input
             value={num}
